a1093adaa3a7c6a5f2f8843f16d9fcf67557b468
Konstantin Unruh <freaxmate@protonmail.com>
2025-05-14 15:47:22 +0200

    update

---
+++ Mesh.py
@@ -0,0 +1,32 @@
+
+
+class Triangle:
+    def __init__(self, node_1, node_2, node_3):
+        pass
+
+class Mesh:
+    def __init__(self, coordinates, nodes):
+        pass
+
+    # Task 2
+    def determinant(self, triangle):
+        pass
+
+    # Task 3
+    def minimum_angle(self, triangle):
+        pass
+
+    # Task 4
+    def list_determinants(self):
+        pass
+
+    # Task 5
+    def integral(self, function):
+        pass
+
+    # Task 6
+    def total_area(self):
+        pass
+
+if __name__ == "__main__":
+    pass
