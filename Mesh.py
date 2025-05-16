08d7bf4744634a90169e9281a3786095bb58454f
Konstantin Unruh <freaxmate@protonmail.com>
2025-05-16 19:54:18 +0200

    update

--- Mesh.py
+++ Mesh.py
@@ -1,31 +1,120 @@
-
-
 class Triangle:
     def __init__(self, node_1, node_2, node_3):
+        """
+        Explanation
+
+        Arguments:
+            node_1: Explanation
+            node_2: Explanation
+            node_3: Explanation
+
+        Returns:
+            None
+        """
+        pass
+
+    def get_nodes(self, node_type):
+        """
+        Explanation
+
+        Arguments:
+            node_type: Explanation
+
+        Returns:
+            None
+        """
         pass
 
 class Mesh:
     def __init__(self, coordinates, nodes):
+        """
+        Explanation
+
+        Arguments:
+            coordinates: Explanation
+            nodes: Explanation
+
+        Returns:
+            None
+        """
         pass
 
     # Task 2
     def determinant(self, triangle):
+        """
+        Explanation
+
+        Arguments:
+            triangle: Explanation
+
+        Returns:
+            None
+        """
         pass
 
     # Task 3
     def minimum_angle(self, triangle):
+        """
+        Explanation
+
+        Arguments:
+            triangle: Explanation
+
+        Returns:
+            None
+        """
         pass
 
     # Task 4
     def list_determinants(self):
+        """
+        Explanation
+
+        Arguments:
+            None
+
+        Returns:
+            None
+        """
         pass
 
     # Task 5
     def integral(self, function):
+        """
+        Explanation
+
+        Arguments:
+            function: Explanation
+
+        Returns:
+            None
+        """
         pass
 
     # Task 6
     def total_area(self):
+        """
+        Explanation
+
+        Arguments:
+            None
+
+        Returns:
+            None
+        """
+        pass
+
+    # Task 7
+    def draw(self):
+        """
+        Explanation
+
+        Arguments:
+            None
+
+        Returns:
+            None
+        """
         pass
 
 if __name__ == "__main__":
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "mkdocs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self , nixpkgs ,... }:
+  let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default =
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      #LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+      packages = with pkgs; [
+        python312Packages.python
+        python312Packages.matplotlib
+        python312Packages.notebook
+        python312Packages.numpy
+        python312Packages.scipy
+        python312Packages.pandas
+        python312Packages.ipykernel
+        python312Packages.ipython
+        python312Packages.ipywidgets
+        python312Packages.jupyterlab-widgets
+        python312Packages.ipympl
+        python312Packages.seaborn
+      ];
+    };
+  };
+}
